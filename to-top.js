541eaa63d3c3617dfe4103bb557c5968b2b3be9f
Yoann Joly <yoann.joly12@gmail.com>
2018-12-19 16:47:26 +0100

    top

--- to-top.js
+++ to-top.js
@@ -1,10 +1,10 @@
 jQuery(function(){
     $(function () {
-    $(window).scroll(function () { //Fonction appelée quand on descend la page
-    if ($(this).scrollTop() > 140 ) {  // Quand on est à 140 pixels du haut de page,
-    $('#scrollUp').css('right','10px'); // Replace à 10pixels de la droite l'image
+    $(window).scroll(function () { 
+    if ($(this).scrollTop() > 140 ) {  
+    $('#scrollUp').css('right','10px'); 
     } else { 
-    $('#scrollUp').removeAttr( 'style' ); // Enlève les attributs CSS affectés par javascript
+    $('#scrollUp').removeAttr( 'style' ); 
     }
     });
     });
