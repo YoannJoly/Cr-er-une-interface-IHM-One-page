bbc19cc9e5d6c61e1740cdee93696690873c7c87
Yoann Joly <yoann.joly12@gmail.com>
2018-12-19 16:55:03 +0100

    fin niveau2

--- to-top.js
+++ to-top.js
@@ -10,13 +10,13 @@ jQuery(function(){
     });
     });
      
-    $(document).ready(function(){
-        
-        $('a').on('click', function(evt){           
-           evt.preventDefault();            
-        var target = $(this).attr('href');           
-        $('html, body')           
-           .stop()           
-           .animate({scrollTop: $(target).offset().top}, 1000 );
-        });
+$(document).ready(function(){
+    
+    $('a').on('click', function(evt){           
+        evt.preventDefault();            
+    var target = $(this).attr('href');           
+    $('html, body')           
+        .stop()           
+        .animate({scrollTop: $(target).offset().top}, 1000 );
     });
+});
