83de685f4fa206888e8021900e14ab70ac59de66
Yoann Joly <yoann.joly12@gmail.com>
2018-12-19 17:03:15 +0100

    fin j2

---
+++  mesfonctions.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    
+    $('a').on('click', function(evt){           
+        evt.preventDefault();            
+    var target = $(this).attr('href');           
+    $('html, body')           
+        .stop()           
+        .animate({scrollTop: $(target).offset().top}, 1000 );
+    });
+});
--- to-top.js
+++ to-top.js
@@ -9,14 +9,5 @@ jQuery(function(){
     });
     });
     });
-     
-$(document).ready(function(){
+  
     
-    $('a').on('click', function(evt){           
-        evt.preventDefault();            
-    var target = $(this).attr('href');           
-    $('html, body')           
-        .stop()           
-        .animate({scrollTop: $(target).offset().top}, 1000 );
-    });
-});
