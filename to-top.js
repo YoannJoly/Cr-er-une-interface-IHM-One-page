f0cdab5f6dcbf37738592d2be28cec9c3131131a
Yoann Joly <yoann.joly12@gmail.com>
2019-01-15 11:50:27 +0100

    standard JS

--- to-top.js
+++ to-top.js
@@ -1,26 +1,25 @@
-jQuery(function(){
-    $(function () {
-    $(window).scroll(function () { 
-    if ($(this).scrollTop() > 140 ) {  
-    $('#scrollUp').css('right','10px'); 
-    } else { 
-    $('#scrollUp').removeAttr( 'style' ); 
+jQuery(function () {
+  $(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 140) {
+        $('#scrollUp').css('right', '10px')
+      } else {
+        $('#scrollUp').removeAttr('style')
+      }
+    })
+  })
+})
+
+$(function () {
+  $("a[href*='#']:not([href='#'])").click(function () {
+    if (
+      location.hostname === this.hostname && this.pathname.replace(/^\//, '') === location.pathname.replace(/^\//, '')
+    ) {
+      var anchor = $(this.hash)
+      anchor = anchor.length ? anchor : $('[name=' + this.hash.slice(1) + ']')
+      if (anchor.length) {
+        $('html, body').animate({ scrollTop: anchor.offset().top }, 1500)
+      }
     }
-    });
-    });
-    });
-  
-$(function() {
-    
-    $("a[href*='#']:not([href='#'])").click(function() {
-        if (
-            location.hostname == this.hostname&& this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
-        ) {
-            var anchor = $(this.hash);
-            anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
-            if ( anchor.length ) {
-                $("html, body").animate( { scrollTop: anchor.offset().top }, 1500);
-            }
-        }
-    });
-});  
+  })
+})
